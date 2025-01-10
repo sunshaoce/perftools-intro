6238e889d51d2077c71ddf45547681544a4b1ce4
Shao-Ce SUN <sunshaoce@outlook.com>
2025-01-10 17:40:57 +0800

    update

--- linux-perf/sample1/sample.c
+++ linux-perf/sample1/sample.c
@@ -1,61 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct PrimeNumberNode
-{
-    int prime;
-    int count;
-    struct PrimeNumberNode *next;
+struct PrimeNumberNode {
+  int prime;
+  int count;
+  struct PrimeNumberNode *next;
 };
 
-int wasteTime(int factor)
-{
-    int i = 0;
-    int testResult;
-    for (i = 0; i < factor; i++)
-    {
-        testResult += i;
-        testResult = testResult * 1;
-    }
-    return testResult;
+int wasteTime(int factor) {
+  int i = 0;
+  int testResult;
+  for (i = 0; i < factor; i++) {
+    testResult += i;
+    testResult = testResult * 1;
+  }
+  return testResult;
 }
 
 //  function to get prime number which is very hot
-struct PrimeNumberNode *number(int input)
-{
-    int i;
-    int j;
+struct PrimeNumberNode *number(int input) {
+  int i;
+  int j;
 
-    struct PrimeNumberNode *head = (struct PrimeNumberNode *)malloc(sizeof(struct PrimeNumberNode));
-    struct PrimeNumberNode *currentNode = head;
-    for (i = 3; i < input; i++)
-    {
-        if (i % 2 == 1)
-        {
-            for (j = 2; j < i; j++)
-            {
-                int testResult = wasteTime(j);
-                if (testResult % 10000 == 9999)
-                    printf("Test code");
-                if (i % j == 0)
-                    continue;
-            }
-            struct PrimeNumberNode *primeNode = (struct PrimeNumberNode *)malloc(sizeof(struct PrimeNumberNode));
-            currentNode->next = primeNode;
-            currentNode = primeNode;
-            head->count++;
-        }
+  struct PrimeNumberNode *head =
+      (struct PrimeNumberNode *)malloc(sizeof(struct PrimeNumberNode));
+  struct PrimeNumberNode *currentNode = head;
+  for (i = 3; i < input; i++) {
+    if (i % 2 == 1) {
+      for (j = 2; j < i; j++) {
+        int testResult = wasteTime(j);
+        if (testResult % 10000 == 9999)
+          printf("Test code");
+        if (i % j == 0)
+          continue;
+      }
+      struct PrimeNumberNode *primeNode =
+          (struct PrimeNumberNode *)malloc(sizeof(struct PrimeNumberNode));
+      currentNode->next = primeNode;
+      currentNode = primeNode;
+      head->count++;
     }
+  }
 
-    return head;
+  return head;
 }
 
-int main(int argc, char *argv[])
-{   
-    printf("input=%d\n", atoi(argv[1]));
-    struct PrimeNumberNode* head = number(atoi(argv[1]));
-    printf("%d\n",head->count);
+int main(int argc, char *argv[]) {
+  printf("input=%d\n", atoi(argv[1]));
+  struct PrimeNumberNode *head = number(atoi(argv[1]));
+  printf("%d\n", head->count);
 
-    head = number(atoi(argv[1]));
-    printf("%d\n",head->count);
+  head = number(atoi(argv[1]));
+  printf("%d\n", head->count);
 }
--- linux-perf/sample2/mem.c
+++ linux-perf/sample2/mem.c
@@ -1,27 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-	int n = atoi(argv[1]);
-	int enough = 1;
-        while(1)
-        {
-		
-		// use 1MB memory
-        	if (enough) {        
-			void *m = malloc(1024*1024);
-                	if (!m) {
-	           		enough = 0;
-		   		printf("No more memory to use");
-			}
-			else {
-		   		memset(m,0,1024*1024);
-			}
-		}
-		// sleep n micro seconds
-		usleep(n);
-        }
-        return 0;
+int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]);
+  int enough = 1;
+  while (1) {
+    // use 1MB memory
+    if (enough) {
+      void *m = malloc(1024 * 1024);
+      if (!m) {
+        enough = 0;
+        printf("No more memory to use");
+      } else {
+        memset(m, 0, 1024 * 1024);
+      }
+    }
+    // sleep n micro seconds
+    usleep(n);
+  }
+  return 0;
 }
